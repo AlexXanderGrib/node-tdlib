89a28be5ad81561b0ad2af1e3129a00b956907b4
Alexander Gribochkin <me@alexxgrib.me>
2024-10-20 00:15:41 +0300

    fix: test-linux-arm64.sh

--- scripts/test-linux-arm64.sh
+++ scripts/test-linux-arm64.sh
@@ -7,7 +7,7 @@ echo $ID;
 if [ "$ID" = "alpine" ]; then
   apk add --update --no-cache python3 py3-pip g++ make py3-pip && ln -sf python3 /usr/bin/python
   npm ci
-  TDLIB_PATH=$(readlink -f ./packages/tdjson-linux-arm64-glibc/libtdjson-arm64-musl.so) npm test -- --run
+  TDLIB_PATH=$(readlink -f ./packages/tdjson-linux-arm64-musl/libtdjson-arm64-musl.so) npm test -- --run
 else
   npm ci
   TDLIB_PATH=$(readlink -f ./packages/tdjson-linux-arm64-glibc/libtdjson-arm64-glibc.so) npm test -- --run
