80b35eaacc1e0e6268d9922e3d0f79aafa7ad3c4
AlexXanderGrib <AlexXanderGrib@users.noreply.github.com>
2024-10-19 19:18:13 +0000

    [create-pull-request] automated change

---
+++ packages/tdjson-win32-ia32/index.js
@@ -0,0 +1,14 @@
+/**
+ * @type {string}
+ */
+module.exports.tdlibPath = require('path').resolve(__dirname, "tdjson-x32.dll");
+/**
+ * @type {string}
+ * @default "1.8.37"
+ */
+module.exports.version = "1.8.37";
+/**
+ * @type {string}
+ * @default "c1fea0f26355d94842f25651edc154842563e97b"
+ */
+module.exports.commit = "c1fea0f26355d94842f25651edc154842563e97b";
