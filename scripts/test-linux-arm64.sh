c521bede2a7890ea6344df709f88461034793478
Alexander Gribochkin <me@alexxgrib.me>
2024-10-19 23:02:39 +0300

    fix: ci make linux test script install python on alpine

--- scripts/test-linux-arm64.sh
+++ scripts/test-linux-arm64.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+source /etc/os-release
+
+if [ $ID = "alpine" ]; then
+  apk add --update --no-cache python3 py3-pip && ln -sf python3 /usr/bin/python
+  python3 -m ensurepip  
+fi
+
 npm ci
 TDLIB_PATH=$(readlink -f ./packages/tdjson-linux-arm64-glibc/libtdjson-arm64-glibc.so) npm test -- --run
